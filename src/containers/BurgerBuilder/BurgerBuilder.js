2cb3fa7344a63146480ca4e4fd6042f139df3346
Andrew Ashton <andrew.ashton@vizexplorer.com>
2019-03-29 14:24:47 +1300

    Remove ingredients safely

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -37,15 +37,36 @@ class BurgerBuilder extends Component {
   }
 
   removeIngredientHandler = (type) => {
+    const oldCount = this.state.ingredients[type];
+    if (oldCount === 0) {
+      return;
+    }
+    const newCount = oldCount - 1;
+    const updatedIngredients = {
+      ...this.state.ingredients
+    };
+    updatedIngredients[type] = newCount;
+    this.setState({
+      ingredients: updatedIngredients,
+      totalPrice: this.state.totalPrice - INGREDIENT_PRICES[type],
 
+    })
   }
 
   render() {
+    const disabledInfo = {
+      ...this.state.ingredients
+    };
+    for (let key in disabledInfo) {
+      disabledInfo[key] = disabledInfo[key] === 0;
+    }
     return (
       <Aux>
         <Burger ingredients={this.state.ingredients} />
         <BuildControls
-          ingredientAdded={this.addIngredientHandler} />
+          ingredientAdded={this.addIngredientHandler}
+          ingredientRemoved={this.removeIngredientHandler}
+          disabled={disabledInfo} />
       </Aux>
     );
   }
